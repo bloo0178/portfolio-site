13e2043a17fecb98277dc860e513f86b399b814d
bloo0178 <bloo0178@gmail.com>
2019-02-10 21:28:31 -0500

    Minor aesthetic updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import styles from "./Hero.module.scss";
 
-class Hero extends React.Component {
+const Hero = props => {
+
+  return (
+    <div className={styles.container}>
+        <div className={styles.title}>
+          <h1>Jeff Bloom</h1>
+        </div>
+        <div>put icons for github, linkedin here</div>
+        <div>Add arrow at the bottom, or parallax effect</div>
+        <div>Allow black page to dip into top of white as a triangle</div>
+        <div>Use a dotted line with a triangle to do the same from About to Projects</div>
+        <div>Make ABOUT page 100vh</div>
+    </div>
+  );
+};
+
+/*class Hero extends React.Component {
+  
   render() {
     return (
       <div className={styles.container}>
@@ -13,6 +30,6 @@ class Hero extends React.Component {
       </div>
     );
   }
-}
+}*/
 
 export default Hero;
