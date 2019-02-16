54b4ff366a63e9ac9c60a767c3d3b59fd0912af3
bloo0178 <bloo0178@gmail.com>
2019-02-16 16:04:20 -0500

    Font and style updates.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./Hero.module.scss";
 import { IoMdArrowDown } from "react-icons/io";
 
-
 class Hero extends React.Component {
   state = {
     currentScrollHeight: 0
@@ -22,15 +21,31 @@ class Hero extends React.Component {
     return (
       <div className={styles.container}>
         <div className={styles.title} style={{ opacity }}>
-          <h2>Full Stack Dev / Solutions Architect</h2>
+          <h1>Full Stack Dev / Solutions Architect</h1>
         </div>
         <div style={{ opacity }}>
           <p>
-            Lorem ipsum dolor sit amet, congue tincidunt cu nec, diam exerci id
-            sed. Ne pri errem alterum, ne quis quaerendum est, dico inimicus his
-            te. Fierent urbanitas vel te, in soleat commune tacimates est. Solum
-            populo dissentiet sed et, eu fugit oblique eam. Nullam quodsi
-            appellantur ad usu.
+            My name is <strong>Jeff Bloom</strong>. I am passionate about
+            all things software, and currently work heavily with the MERN stack
+            (MongoDB, Express, React, & Node).
+          </p>
+          <p>
+            Prior to following a software development career path, I built 10+
+            years of experience as both a Systems Analyst within a large, global
+            corporation and most recently as a Solutions Architect at a
+            nationally-recognized digital business consultancy.
+          </p>
+
+          <p>
+            My background has provided me firsthand insights into many different
+            aspects of the IT, custom software, and business technology worlds -
+            and also how they all tie together in the context of larger
+            objectives.
+          </p>
+          <p>
+            This site is intended to serve as a showcase of my technical
+            skillset. If my background and capabilities interest you or you'd
+            like to learn more, let's connect and talk further!
           </p>
         </div>
         <IoMdArrowDown
