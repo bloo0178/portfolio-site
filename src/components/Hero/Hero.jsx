73f412a2f2ef3fe68954c96acb6ff52552887de2
bloo0178 <bloo0178@gmail.com>
2019-04-03 21:06:14 -0400

    Reworked project cards and color palette.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import styles from "./Hero.module.scss";
-//import HeroImage from '../HeroImage';
 
 class Hero extends React.Component {
   render() {
     return (
-        <div className={styles.container}>
-          <div className={styles.title}>
-            {/*eslint-disable-next-line*/}
-            <h1>{`Jeff Bloom`}</h1>
-            {/*eslint-disable-next-line*/}
-            <h2>// Full Stack Dev</h2>
-          </div>
-          <div className={styles.left} />
-          <div />
+      <div className={styles.container}>
+        <div className={styles.title}>
+          {/*eslint-disable-next-line*/}
+          <h1>{`Jeff Bloom`}</h1>
+          {/*eslint-disable-next-line*/}
+          <h2>// Full Stack Dev</h2>
         </div>
+      </div>
     );
   }
 }
