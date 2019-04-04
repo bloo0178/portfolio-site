6f16c790e07c767e349baec80b325492cc5437d1
Jeff Bloom <bloo0178@gmail.com>
2019-04-04 18:50:16 -0400

    Updated About section.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,24 +9,23 @@ export class About extends React.Component {
         <div className={styles.content}>
           <p>
             I am obsessed with the infinite pursuit of learning and applying
-            technology to evolve the things we do and the way we do them. My
+            technology to evolve the things we do. My
             personal passion is software development.
           </p>
           <p>
             I have over a decade of professional experience mixing equal parts
-            systems and design thinking. I have worked as a Business
-            Systems Analyst for multiple global organizations, and most recently
+            systems and design thinking. Most recently I worked
             as a Solutions Architect at an award-winning digital business
-            consultancy. I find being a part of deeply-engaged teams very
-            rewarding, and have extensive experience leading and engaging in
+            consultancy. I find being part of deeply-engaged teams very
+            rewarding, and have extensive experience leading and taking part in
             complex efforts inclusive of UX, BA, Dev, QA, and PM talent.
           </p>
           <p>
             My work with dozens of organizations has provided me a practical
             view into the relationship between user needs and business goals.
             This has taught me to avoid technology bias - leverage what makes
-            sense within the context of the situation and always embrace
-            innovation that sparks an evolution.
+            sense within the context of the situation and embrace
+            innovation that sparks evolution.
           </p>
         </div>
       </div>
