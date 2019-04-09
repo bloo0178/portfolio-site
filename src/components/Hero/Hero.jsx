e9645335a62035099f084a04c6cb0405f7cd750e
Jeff Bloom <bloo0178@gmail.com>
2019-04-09 10:41:38 -0400

    Added scalable SVG logo.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import styles from "./Hero.module.scss";
-import logo from "./images/logo2.svg";
-import logo2 from "./images/logo3.svg";
-
-// Trying to get logo to scale with screensize. Viewbox/ port = vh/ vw?
+import logo from "./images/logo.svg";
 
 class Hero extends React.Component {
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.logo}>
-          {/*<img src={logo} alt="" width="300px" />*/}
-        </div>
-        <div className={styles.test}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100%" height="100%">
-            <ellipse fill="white" cx="50" cy="50" rx="50" ry="50" />
-          </svg>
+          <img src={logo} width="100%" height="100%" />
         </div>
       </div>
     );
