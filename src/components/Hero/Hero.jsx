100c169a8a4de4c5f16360e27328f64b3503e935
Jeff Bloom <bloo0178@gmail.com>
2019-04-08 21:29:17 -0400

    Worked on new logo and getting SVG to scale. Cleaned up CSS that was cutting accross elements (background-color defined in App, etc.). Still need to add actual logo in.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./Hero.module.scss";
+import logo from "./images/logo2.svg";
+import logo2 from "./images/logo3.svg";
+
+// Trying to get logo to scale with screensize. Viewbox/ port = vh/ vw?
 
 class Hero extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.title}>
-          {/*eslint-disable-next-line*/}
-          <h1>{`Jeff Bloom`}</h1>
-          {/*eslint-disable-next-line*/}
-          <h2>// Full Stack Dev</h2>
+        <div className={styles.logo}>
+          {/*<img src={logo} alt="" width="300px" />*/}
+        </div>
+        <div className={styles.test}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100%" height="100%">
+            <ellipse fill="white" cx="50" cy="50" rx="50" ry="50" />
+          </svg>
         </div>
       </div>
     );
