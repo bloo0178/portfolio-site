9aaf8ceeab450d0560df74e1e8067ec26be8d737
Jeff Bloom <bloo0178@gmail.com>
2019-04-22 08:28:03 -0400

    Navbar improvement

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
-import { IoLogoLinkedin, 
-  IoLogoGithub, 
- } from "react-icons/io";
+import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
 
 class NavBar extends React.Component {
-
   render() {
     return (
       <nav className={styles.navbar}>
-      <div className={styles.iconsContainer}>
-        <a href={"https://www.linkedin.com/in/bloomjeff/"}><IoLogoLinkedin className={styles.icon} /></a>
-        <a href={"https://github.com/bloo0178"}><IoLogoGithub className={styles.icon} /></a>
+        <div className={styles.iconsContainer}>
+          <a href={"https://www.linkedin.com/in/bloomjeff/"}>
+            <IoLogoLinkedin className={styles.icon} />
+          </a>
+          <a href={"https://github.com/bloo0178"}>
+            <IoLogoGithub className={styles.icon} />
+          </a>
         </div>
       </nav>
-    )
+    );
   }
 }
 
